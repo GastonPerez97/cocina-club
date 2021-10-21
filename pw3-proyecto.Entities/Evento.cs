13d5403056a24a5a20887e8e888ea4c9cb67e2e9
Gaston Perez <gastonperez-97@hotmail.com>
2021-10-20 23:16:44 -0300

    added MetaData folder and MetaData models. gitignore event images

--- pw3-proyecto.Entities/Evento.cs
+++ pw3-proyecto.Entities/Evento.cs
@@ -1,9 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.ComponentModel.DataAnnotations;
-using pw3_proyecto.Common.CustomDataAnnotations;
-
 
 #nullable disable
 
@@ -20,39 +16,17 @@ namespace pw3_proyecto.Entities
 
         public int IdEvento { get; set; }
         public int IdCocinero { get; set; }
-
-        [Required(ErrorMessage = "El campo Nombre es obligatorio.")]
-        [StringLength(50, ErrorMessage = "El nombre no puede tener más de 50 caracteres.")]
         public string Nombre { get; set; }
-
-        [Required(ErrorMessage = "El campo Fecha es obligatorio.")]
-        [CurrentDateOrHigher(ErrorMessage = "La fecha debe ser mayor a la actual.")]
         public DateTime Fecha { get; set; }
-
-        [DisplayName("Cantidad de comensales")]
-        [Required(ErrorMessage = "El campo Cantidad de comensales es obligatorio.")]
-        [Range(1, int.MaxValue, ErrorMessage = "La cantidad de comensales tiene que ser mayor a 0.")]
         public int CantidadComensales { get; set; }
-
-        [DisplayName("Ubicación")]
-        [Required(ErrorMessage = "El campo Ubicación es obligatorio.")]
         public string Ubicacion { get; set; }
-
         public string Foto { get; set; }
-
-        [Required(ErrorMessage = "El campo Precio es obligatorio.")]
-        [DataType(DataType.Currency)]
-        [Range(1.00D, double.MaxValue, ErrorMessage = "Un evento no puede ser gratuito.")]
         public decimal Precio { get; set; }
-
         public int Estado { get; set; }
 
         public virtual Usuario IdCocineroNavigation { get; set; }
         public virtual ICollection<Calificacione> Calificaciones { get; set; }
         public virtual ICollection<Reserva> Reservas { get; set; }
-
-        [DisplayName("Recetas para el evento")]
-        [Required(ErrorMessage = "El campo Recetas es obligatorio, selecciona al menos una.")]
         public virtual ICollection<EventosReceta> EventosReceta { get; set; }
     }
 }
