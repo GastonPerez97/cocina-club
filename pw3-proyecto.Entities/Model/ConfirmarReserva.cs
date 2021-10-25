0d493906bbb560638de2590ec75f193de4108607
Mariano Perri <marianoperri@gmail.com>
2021-10-25 20:18:25 -0300

    agregando logica para reservar un evento

---
+++ pw3-proyecto.Entities/Model/ConfirmarReserva.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+namespace pw3_proyecto.Entities.Model
+{
+    public class ConfirmarReserva
+    {
+        public int IdEvento { get; set; }
+        public int IdComensal { get; set; }
+        [DisplayName("Cuantos vienen a comer?")]
+        [Required]
+        public int CantidadComensales { get; set; }
+        
+        [DisplayName("Elegi tu menu")]
+        public List<Receta> recetas { get; set; }
+        
+        [Required(ErrorMessage = "Tenes que seleccionar una receta")]
+        public int IdRecetaElegida { get; set; }
+    }
+}
