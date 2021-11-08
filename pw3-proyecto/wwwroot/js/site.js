042b3a7edb5072526f79dd036c181450dc13b2a0
Gaston Perez <gastonperez-97@hotmail.com>
2021-11-07 21:46:04 -0300

    modified reservas view

--- pw3-proyecto/wwwroot/js/site.js
+++ pw3-proyecto/wwwroot/js/site.js
@@ -1,9 +1,4 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
-
-// Write your JavaScript code.
-
-$(document).on("click", "#btnModal", function () {
+﻿$(document).on("click", "#btnModal", function () {
     var evento = $(this).data('even');
     var comensal = $(this).data('comen');
 
