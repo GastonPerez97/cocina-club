056f68e0c662b5cf659288811862b90a643b8cb5
Gaston Perez <gastonperez-97@hotmail.com>
2021-11-14 17:40:38 -0300

    modified reservas view

--- pw3-proyecto/wwwroot/js/reservas.js
+++ pw3-proyecto/wwwroot/js/reservas.js
@@ -1,7 +1,14 @@
-﻿$(document).on("click", "#btnModal", function () {
+﻿// New review modal button
+$(document).on("click", "#btnModal", function () {
     var evento = $(this).data('even');
     var comensal = $(this).data('comen');
 
     $("#eventoIdjs").val(evento);
     $("#comensalIdJs").val(comensal);
 });
+
+// Review comments popover
+var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+    return new bootstrap.Popover(popoverTriggerEl)
+})
